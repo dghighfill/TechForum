f73672b917518b0a896667c534097de3deda4869
Dale Highfill <dghighfill@dstsystems.com>
2011-11-07 07:07:00 -0600

    Changes to support conditional configuration

--- TechForum/src/com/dstsystems/techforum/SkipListener.java
+++ TechForum/src/com/dstsystems/techforum/SkipListener.java
@@ -2,6 +2,8 @@ package com.dstsystems.techforum;
 
 import java.util.logging.Logger;
 
+import org.springframework.batch.core.annotation.OnSkipInProcess;
+import org.springframework.batch.core.annotation.OnSkipInRead;
 import org.springframework.batch.core.annotation.OnSkipInWrite;
 
 import com.dstsystems.techforum.beans.Customer;
@@ -10,6 +12,16 @@ public class SkipListener
 {
    private Logger logger = Logger.getLogger( SkipListener.class.getName() );
    
+   @OnSkipInRead
+   public void onSkipInRead( Throwable throwable )
+   {
+	   logger.info( "Customer is getting skipped.\n" + throwable.getMessage() );
+   }
+   @OnSkipInProcess
+   public void onSkipInProcess( Customer customer, Throwable throwable ) 
+   {
+	   logger.info( "Customer: " + customer.getId() + " is getting skipped.\n" + throwable.getMessage() );
+   }
    @OnSkipInWrite
    public void onSkipInWrite( Customer customer, Throwable throwable )
    {
